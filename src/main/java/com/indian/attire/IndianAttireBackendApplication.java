e13adb254fd42e94d5bcd5fa2e47536bd231072a
rampujan <rampujan1999@gmail.com>
2024-12-16 22:07:03 +0530

    first commit

---
+++ src/main/java/com/indian/attire/IndianAttireBackendApplication.java
@@ -0,0 +1,13 @@
+package com.indian.attire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianAttireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndianAttireBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indian/attire/IndianAttireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indian.attire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndianAttireBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
